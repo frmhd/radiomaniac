2c64d76d01eaf1f4f0462091e98164d07eecd073
Dmitriy Tokarev <freemiumhead@gmail.com>
2017-12-04 02:03:36 +0300

    write to file

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const osmosis = require('osmosis');
+const cheerio = require('cheerio');
+const axios = require('axios');
+const R = require('ramda');
+const fs = require('fs');
 
 const formatYesterday = () => {
   const today = new Date();
   const yesterday = new Date(today).setDate(today.getDate() - 1);
-  const yesterdayDate = () => new Date(yesterday);
-  
-  const getYear = yesterdayDate().getFullYear();
-  const getMonth = yesterdayDate().getMonth();
-  const getDay = yesterdayDate().getDate();
-  const formatDay = getDay.toString().length < 2 ? `0${getDay}` : getDay;
-  
-  return `${getYear}-${getMonth}-${formatDay}`;
+
+  const getYear = new Date(yesterday).getFullYear();
+  const getMonth = new Date(yesterday).getMonth() + 1;
+  const getDay = new Date(yesterday).getDate();
+  const formatDate = date => date.toString().length < 2 ? `0${date}` : date;
+
+  return `${getYear}-${formatDate(getMonth)}-${formatDate(getDay)}`;
 };
 
 const getUrl = () => `http://www.europaplus.ru/index.php?go=Playlist&date=${formatYesterday()}&time_start=00.00&time_stop=23.59&channel=europa&__ajax__=1&list_only=1`;
 
-osmosis
-  .get(getUrl())
-  .data((data) => {
-      const artists = data.artist;
-      const formatData = data.song.map((item, index) => ({
-          'song': item,
-          'artist': artists[index]
-        }));
-        return console.log(formatData);
-      })
-  .log(console.log)
-  // .error(console.log)
-  // .debug(console.log)
+axios.get(getUrl()).then(data => {
+  const $ = cheerio.load(data.data);
+  const tracks = $('.jp_container').map((i, elem) => {
+    const track = {
+      time: $(elem).find('.time').text(),
+      artist: $(elem).find('.jp-title .title').text(),
+      song: $(elem).find('.jp-title span').text()
+    }
+    
+    return track;
+  });
+
+  R.map((i, elem) => fs.appendFileSync("hello.csv", `${elem.time}, ${elem.song}, ${elem.artist}\n`), tracks);
+
+});
