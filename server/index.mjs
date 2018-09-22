61145b8a96b8783a2310718d235f73d188f76e4e
Dmitriy Tokarev <freemiumhead@gmail.com>
2018-09-22 11:57:53 +0300

    change time

--- server/index.mjs
+++ server/index.mjs
@@ -38,7 +38,7 @@ MongoClient.connect(mongoToken, (err, database) => {
 
     const bot = new TelegramBot(telegramToken, { polling: true })
 
-    const sendDataToChat = () => new Cron.CronJob('00 56 11 * * 6', async () => {
+    const sendDataToChat = () => new Cron.CronJob('00 00 10 * * 1', async () => {
       await europaPlusPost()
       console.log('EUROPE data added to Mongo')
       await nasheRadioPost()
