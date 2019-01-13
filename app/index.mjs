d911d953cd3edb9e147f2cd992566746ad9007cc
Dmitriy Tokarev <freemiumhead@gmail.com>
2019-01-14 01:58:10 +0300

    change cron time

--- app/index.mjs
+++ app/index.mjs
@@ -6,7 +6,7 @@ import start from './utils/start';
 const app = express();
 const port = 8000;
 
-const startApp = () => new Cron.CronJob('00 00 02 * * 1', start, null, true, 'Europe/Moscow');
+const startApp = () => new Cron.CronJob('00 00 10 * * 1', start, null, true, 'Europe/Moscow');
 
 app.listen(port, () => {
   startApp();
